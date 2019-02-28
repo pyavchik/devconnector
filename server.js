faa3b3961229311c75dcf1fa97304d21554949b4
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-28 21:44:50 +0200

    mongo db added

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to MongoDb
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.log(error));
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Hello world!')
 });
 
 const port = process.env.PORT || 5000;
